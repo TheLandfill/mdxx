c201bffd2ff13755d9311fdbfb512730b123af57
Joseph Mellor <the.landfill.coding@gmail.com>
2019-08-13 11:50:48 -0500

    Added {{zs}} for Zero-Width Space

--- tumd/html/default.py
+++ tumd/html/default.py
@@ -4,6 +4,7 @@ default_dict = {
     'empty': u"\u0007",             # The 'bell' character hasn't been used in
                                     # twenty years and it wouldn't even work in
                                     # html anyway
+    'zs' : '&#8203;',               # Zero-width space
     '{' : '<code>',
     '}' : '</code>',
     r'\{' : '{',
