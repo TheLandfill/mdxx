1b964821a6ff304522f1f0699917ff6ffdf32fc4
Joseph Mellor <the.landfill.coding@gmail.com>
2020-03-02 09:32:25 -0600

    Added Redirection Templates

--- tumd/html/redirect.py
+++ tumd/html/redirect.py
@@ -2,6 +2,8 @@
 
 def get_link(args):
     template = args[0]
+    tumd = template.template
     metadata_tags = [(r'Redirect Link', True)]
     data = template.get_tags(metadata_tags)
     tumd.cur_context_vars()['redir'] = data[0]
+    return ''
