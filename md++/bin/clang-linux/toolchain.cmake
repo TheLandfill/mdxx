ab0fc8811c14bf239271991d0f9f7643b6dbb03c
Joseph Mellor <the.landfill.coding@gmail.com>
2021-01-17 02:41:55 -0600

    Added clang-linux toolchain

---
+++ md++/bin/clang-linux/toolchain.cmake
@@ -0,0 +1,20 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_CROSSCOMPILING TRUE)
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
+
+add_compile_options(-Wall -Wextra)
+add_compile_options(
+	"$<$<STREQUAL:$<TARGET_PROPERTY:LINKER_LANGUAGE>,CXX>:-std=c++17>"
+	"$<$<STREQUAL:$<TARGET_PROPERTY:LINKER_LANGUAGE>,C>:-std=c99>"
+)
+add_compile_options(
+	"$<$<CONFIG:RELEASE>:-O3>"
+	"$<$<CONFIG:RELEASE>:-ffunction-sections>"
+	"$<$<CONFIG:RELEASE>:-fdata-sections>"
+	"$<$<CONFIG:DEBUG>:-O0>"
+	"$<$<CONFIG:DEBUG>:-g>"
+)
+add_link_options(
+	-ldl -lpthread
+)
