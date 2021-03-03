d021a2f7b4d190b58925e62babdb20c9b6712203
Joseph Mellor <the.landfill.coding@gmail.com>
2021-03-03 00:18:07 -0600

    Added normal Windows toolchain

---
+++ md++/bin/msvc-Windows/toolchain.cmake
@@ -0,0 +1,16 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_VERSION 10.0)
+
+set(CMAKE_C_COMPILER MSVC)
+set(CMAKE_CXX_COMPILER MSVC)
+add_compile_options(/Wall /W4 /std:c++17)
+add_compile_options(
+	"$<$<CONFIG:RELEASE>:/O2>"
+	"$<$<CONFIG:DEBUG>:/Od>"
+	"$<$<CONFIG:DEBUG>:/DEBUG:FASTLINK>"
+)
+add_definitions(
+	"-D_CRT_SECURE_NO_WARNINGS"
+)
+
+set(VS_STARTUP_PROJECT md++)
