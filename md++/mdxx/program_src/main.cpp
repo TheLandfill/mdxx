4e0485461c1c48e91b6b8324916a8e198186d2aa
Joseph Mellor <the.landfill.coding@gmail.com>
2021-03-04 17:36:39 -0600

    Added code that should allow Windows to handle ANSI terminal commands

--- md++/mdxx/program_src/main.cpp
+++ md++/mdxx/program_src/main.cpp
@@ -1,5 +1,19 @@
 #include "run_program.h"
+#ifdef WIN32
+#include <windows.h>
+#define SETUP_ANSI_TERMINAL setup_ansi_terminal();
+void setup_ansi_terminal() {
+	HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+	if (hStdin == INVALID_HANDLE_VALUE) {
+		return;
+	}
+	SetConsoleMode(hStdin, ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+}
+#else
+#define SETUP_ANSI_TERMINAL
+#endif
 
 int main(int argc, char ** argv) {
+	SETUP_ANSI_TERMINAL
 	return MDXX_run_program(argc, argv);
 }
