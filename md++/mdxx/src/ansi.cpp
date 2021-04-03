ed9e471994d5754ef9862de5fa96c28accf52559
Joseph Mellor <the.landfill.coding@gmail.com>
2021-04-02 21:26:56 -0500

    ...Why doesn't it reset right?

--- md++/mdxx/src/ansi.cpp
+++ md++/mdxx/src/ansi.cpp
@@ -3,7 +3,7 @@
 #ifdef WIN32
 const char * MDXX_VAR_COLOR = "\x1b[38;2;128;255;200m";
 const char * MDXX_VAL_COLOR = "\x1b[38;2;255;153;255m";
-const char * MDXX_RESET = "\x1b[0m\x1b[39m";
+const char * MDXX_RESET = "\x1b[0m";
 const char * MDXX_RESET_COLOR = "\x1b[39m";
 const char * MDXX_RESET_FORMATTING = "\x1b[0m";
 const char * MDXX_CONTEXT_COLOR = "\x1b[1m\x1b[38;2;159;135;255m";
@@ -16,7 +16,7 @@ const char * MDXX_CLEAR_LINE = "\r\x1b[K";
 #else
 const char * MDXX_VAR_COLOR = "\033[38;2;128;255;200m";
 const char * MDXX_VAL_COLOR = "\033[38;2;255;153;255m";
-const char * MDXX_RESET = "\033[0m\033[39m";
+const char * MDXX_RESET = "\033[0m";
 const char * MDXX_RESET_COLOR = "\033[39m";
 const char * MDXX_RESET_FORMATTING = "\033[0m";
 const char * MDXX_CONTEXT_COLOR = "\033[1m\033[38;2;159;135;255m";
