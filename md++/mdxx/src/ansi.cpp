0b3b241b818ee873818acce82cb0096bf998aff3
Joseph Mellor <the.landfill.coding@gmail.com>
2021-04-03 05:52:23 -0500

    Possibly simplified ANSI codes since \x1b seems to work on Linux

--- md++/mdxx/src/ansi.cpp
+++ md++/mdxx/src/ansi.cpp
@@ -1,6 +1,5 @@
 #include "mdxx_ansi.h"
 
-#ifdef WIN32
 const char * MDXX_VAR_COLOR = "\x1b[38;2;128;255;200m";
 const char * MDXX_VAL_COLOR = "\x1b[38;2;255;153;255m";
 const char * MDXX_BOLD = "\x1b[1m";
@@ -16,21 +15,4 @@ const char * MDXX_ERROR_PREFIX = "\x1b[1m\x1b[4mERROR:";
 const char * MDXX_WARNING_COLOR = "\x1b[38;2;255;250;205m";
 const char * MDXX_WARNING_PREFIX = "\x1b[1m\x1b[4mWARNING:";
 const char * MDXX_CLEAR_LINE = "\r\x1b[K";
-#else
-const char * MDXX_VAR_COLOR = "\033[38;2;128;255;200m";
-const char * MDXX_VAL_COLOR = "\033[38;2;255;153;255m";
-const char * MDXX_BOLD = "\033[1m";
-const char * MDXX_RESET = "\033[0m";
-const char * MDXX_RESET_COLOR = "\033[39m";
-const char * MDXX_RESET_FORMATTING = "\033[0m";
-const char * MDXX_CONTEXT_COLOR = "\033[1m\033[38;2;159;135;255m";
-const char * MDXX_LINE_COLOR = "\033[1m\033[38;2;254;217;255m";
-const char * MDXX_FILE_COLOR = "\033[38;2;170;196;255m";
-const char * MDXX_FUNC_COLOR = "\033[38;2;175;234;255m";
-const char * MDXX_ERROR_COLOR = "\033[38;2;255;121;0m";
-const char * MDXX_ERROR_PREFIX = "\033[1m\033[4mERROR:";
-const char * MDXX_WARNING_COLOR = "\033[38;2;255;250;205m";
-const char * MDXX_WARNING_PREFIX = "\033[1m\033[4mWARNING:";
-const char * MDXX_CLEAR_LINE = "\r\033[K";
-#endif
 
